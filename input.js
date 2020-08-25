0d58db405ff2df56f28d8adbd43f8c4c83179f0a
sukriti104 <70195660+sukriti104@users.noreply.github.com>
2020-08-25 13:02:57 +0530

    Create input.js

---
+++ input.js
@@ -0,0 +1,50 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "date",
+			alias: "date",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "state",
+            alias: "state",
+            dataType: tableau.dataTypeEnum.string
+
+        }];
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("https://api.covidtracking.com/v1/states/daily.json", function(resp) {
+            var feat = resp,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+					"date": feat[i].date,
+                    "state": feat[i].state
+			
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
